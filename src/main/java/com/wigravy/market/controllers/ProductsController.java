0b6928ffcaee7f4adbdc60abac17c982d1e2aa6c
wigravy <wigravy@gmail.com>
2020-11-04 15:28:00 +0500

    add more bugs and cool things

--- src/main/java/com/wigravy/market/controllers/ProductsController.java
+++ src/main/java/com/wigravy/market/controllers/ProductsController.java
@@ -2,52 +2,57 @@ package com.wigravy.market.controllers;
 
 
 import com.wigravy.market.entities.Product;
-import com.wigravy.market.repositories.specification.ProductSpecification;
-import com.wigravy.market.services.ProductService;
+import com.wigravy.market.services.ProductsService;
+import com.wigravy.market.utils.ProductFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
-    private ProductService productService;
+    private ProductsService productsService;
 
     @Autowired
-    public ProductsController(ProductService productService) {
-        this.productService = productService;
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
     }
 
     @GetMapping
     public String showProductByPage(Model model,
-                                    @RequestParam(name = "page", defaultValue = "1") Integer pageNumber,
-                                    @RequestParam(name = "min_cost", required = false) Double minCost,
-                                    @RequestParam(name = "max_cost", required = false) Double maxCost) {
-
-        Specification<Product> specification = Specification.where(null);
-        if (minCost != null) {
-            specification = specification.and(ProductSpecification.costGreaterOrEqualThan(minCost));
-        }
-        if (maxCost != null) {
-            specification = specification.and(ProductSpecification.costLesserOrEqualThan(maxCost));
-        }
-        List<Product> products = productService.findAll(specification, pageNumber).getContent();
+                                    @RequestParam Map<String, String> requestParams) {
+        Integer pageNumber = Integer.parseInt(requestParams.getOrDefault("page", "1"));
+        ProductFilter productFilter = new ProductFilter(requestParams);
+        Page<Product> products = productsService.findAll(productFilter.getSpec(), pageNumber);
         model.addAttribute("products", products);
+        model.addAttribute("filterDef", productFilter.getFilterDefinition().toString());
         return "all_products";
     }
 
+    @GetMapping("/add")
+    public String showAddForm() {
+        return "add_product_form";
+    }
 
-    // Пример:      http://localhost:8189/app/products/info_by_title?title=Milk
-    @GetMapping("/info_by_title")
-    @ResponseBody
-    public Product infoByTitle(@RequestParam String title) {
-        return productService.findByTitle(title);
+    @PostMapping("/add")
+    public String saveNewProduct(@ModelAttribute Product product) {
+        productsService.saveOrUpdate(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("product", productsService.findById(id));
+        return "edit_product_form";
+    }
+
+    @PostMapping("/edit")
+    public String modifyProduct(@ModelAttribute Product modifiedProduct) {
+        productsService.saveOrUpdate(modifiedProduct);
+        return "redirect:/products";
     }
 }
--- src/main/java/com/wigravy/market/repositories/specification/ProductSpecification.java
+++ src/main/java/com/wigravy/market/repositories/specification/ProductSpecification.java
@@ -4,12 +4,18 @@ package com.wigravy.market.repositories.specification;
 import com.wigravy.market.entities.Product;
 import org.springframework.data.jpa.domain.Specification;
 
+
+
 public class ProductSpecification {
-    public static Specification<Product> costLesserOrEqualThan(Double maxPrice) {
+    public static Specification<Product> priceLesserOrEqualThan(Double maxPrice) {
         return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
     }
 
-    public static Specification<Product> costGreaterOrEqualThan (Double minPrice) {
+    public static Specification<Product> priceGreaterOrEqualThan(Double minPrice) {
         return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+    }
+
+    public static Specification<Product> titleLike(String title) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), String.format("%%%s%%", title));
     }
 }
